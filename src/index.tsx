68517cdca287a8a958b2ae34aa7bda6392108480
N00bG1rl <403helen@gmail.com>
2022-06-16 20:05:39 +0200

    Added redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from 'react-dom'
 
+import { Provider } from 'react-redux'
+import { store } from './state/store'
+
 //import CodeCell from './components/CodeCell'
 import TextEditor from './components/TextEditor'
 
 const App = () => {
-	return <TextEditor />
+	return (
+		// @ts-ignore
+		<Provider store={store}>
+			<TextEditor />
+		</Provider>
+	)
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
