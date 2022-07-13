ff8369335c1769a56faab9fe06ff1df189105135
N00bG1rl <403helen@gmail.com>
2022-07-13 13:07:40 +0300

    Adding a local-only API

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -2,14 +2,25 @@ import path from "path";
 import { Command } from "commander";
 import { serve } from 'local-api'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export const serveCommand = new Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p, --port <number>', 'port to run server on', '4005')
-    .action((filename = 'notebook.js', options: { port: string }) => {
-        console.log(path.join(process.cwd(), path.dirname(filename)))
-        const dir = path.join(process.cwd(), path.dirname(filename))
-
-        console.log(path.basename(filename))
-        serve(parseInt(options.port), path.basename(filename), dir)
+    .action(async (filename = 'notebook.js', options: { port: string }) => {
+        try {
+            //console.log(path.join(process.cwd(), path.dirname(filename)))
+            const dir = path.join(process.cwd(), path.dirname(filename))
+            //console.log(path.basename(filename))
+            await serve(parseInt(options.port), path.basename(filename), dir, !isProduction)
+            console.log(`Opended ${filename}. Navigate to http://localhost:${options.port} to edit the file.`)
+        } catch (err: any) {
+            if (err.code === 'EADDRINUSE') {
+                console.log('Port is in use. Try runnding on a different port.')
+            } else {
+                console.log(err.message)
+            }
+            process.exit(1)
+        }
     })
--- jbook/packages/local-api/dist/index.js
+++ jbook/packages/local-api/dist/index.js
@@ -1,7 +1,26 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
-const serve = (port, filename, dir) => {
-    console.log('Serving, port');
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const serve = (port, filename, dir, useProxy) => {
+    const app = (0, express_1.default)();
+    const packagePath = require.resolve('local-client/build/index.html');
+    app.use(express_1.default.static(path_1.default.dirname(packagePath))); // path.dirname(packagePath)
+    //..node_modules/local-client/build
+    /*  if (useProxy) {
+         app.use(createProxyMiddleware({
+             target: 'http://localhost:3000',
+             ws: true,
+             logLevel: 'silent'
+         }))
+     } else {
+     } */
+    return new Promise((resolve, reject) => {
+        app.listen(port, resolve).on('error', reject);
+    });
 };
 exports.serve = serve;
